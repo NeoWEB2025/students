e06e68f238d94e58fd6bc2b8ec742e9a25f3c8c8
Ivan Denysiuk <ivandenusiuk2006@gmail.com>
2025-05-27 17:54:12 +0200

    i have done dashboard

---
+++ app/components/pagination/Dashboard.tsx
@@ -0,0 +1,25 @@
+export default function Dashboard() {
+    return (
+        <>
+            <div className={`container flex`}>
+                <div className={`left-sidebar`}>
+                    <ul className={`left-sidebar__list flex`}>
+                        <li><a className={`left-sidebar__list--item active`} href="#">Dashboard</a></li>
+                        <li><a className={`left-sidebar__list--item`} href="#">Students</a></li>
+                        <li><a className={`left-sidebar__list--item`} href="#">Tasks</a></li>
+                    </ul>
+                </div>
+
+
+                <main className={`main`}>
+
+                </main>
+            
+            
+                <div className={`right-sidebar`}>
+
+                </div>
+            </div>
+        </>
+    )
+}
