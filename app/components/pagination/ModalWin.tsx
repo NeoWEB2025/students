58d17662360e7d496768f925a47ea190326f230d
Ivan Denysiuk <ivandenusiuk2006@gmail.com>
2025-06-01 01:25:45 +0200

    dasdsa

---
+++ app/components/pagination/ModalWin.tsx
@@ -0,0 +1,13 @@
+export default function ModalWin () {
+    return (
+        <>
+            <div className={`modal`}>
+                <div className={`modal__container`}>
+                    <div className="modal__container--content">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
